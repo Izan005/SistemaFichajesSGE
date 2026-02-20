b9d8bde676da16ae0c4d729473087dc8c7e9e1a6
polete <polgorrindo@gmail.com>
2026-02-20 15:31:45 +0100

    Task Package

---
+++ app/src/main/java/com/example/sistemafichajessge/ui/task/TaskScreen.kt
@@ -0,0 +1,14 @@
+package com.example.sistemafichajessge.ui.task
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.sistemafichajessge.ui.navigation.FichajesNavDestination
+
+object TaskScreen : FichajesNavDestination {
+    override val route = "task"
+}
+
+@Composable
+fun TaskScreen(modifier: Modifier = Modifier) {
+
+}
---
+++ app/src/main/java/com/example/sistemafichajessge/ui/task/TaskViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.sistemafichajessge.ui.task
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
+}
