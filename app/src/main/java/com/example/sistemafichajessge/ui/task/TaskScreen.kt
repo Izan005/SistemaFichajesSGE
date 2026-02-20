2c689f8d4e08e6cf37e808d2e131630289a19204
polete <polgorrindo@gmail.com>
2026-02-20 15:41:38 +0100

    TaskViewModel añadido, ou yeah

--- app/src/main/java/com/example/sistemafichajessge/ui/task/TaskScreen.kt
+++ app/src/main/java/com/example/sistemafichajessge/ui/task/TaskScreen.kt
@@ -1,7 +1,10 @@
 package com.example.sistemafichajessge.ui.task
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sistemafichajessge.ui.navigation.FichajesNavDestination
 
 object TaskScreen : FichajesNavDestination {
@@ -9,6 +12,14 @@ object TaskScreen : FichajesNavDestination {
 }
 
 @Composable
-fun TaskScreen(modifier: Modifier = Modifier) {
+fun TaskScreen(
+    modifier: Modifier = Modifier,
+    viewModel: TaskViewModel = viewModel()
+    ) {
+
+    val taskList by viewModel.taskList.collectAsStateWithLifecycle()
+
+
+
 
 }
--- app/src/main/java/com/example/sistemafichajessge/ui/task/TaskViewModel.kt
+++ app/src/main/java/com/example/sistemafichajessge/ui/task/TaskViewModel.kt
@@ -1,8 +1,87 @@
 package com.example.sistemafichajessge.ui.task
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.application
+import androidx.lifecycle.viewModelScope
+import com.example.sistemafichajessge.data.dbSingleton.FichajesApplication
+import com.example.sistemafichajessge.data.model.Task
+import com.example.sistemafichajessge.data.repository.TaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class TaskViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repo: TaskRepository = (application as FichajesApplication).taskRepo
+
+    val taskList = repo.getAll().stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000L),
+        emptyList()
+    )
+
+    fun insert(task: Task) {
+        viewModelScope.launch {
+            try {
+                repo.insert(task)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    application,
+                    "Insert fallido",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    fun update(task: Task) {
+        viewModelScope.launch {
+            try {
+                repo.update(task)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    application,
+                    "Update fallido",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    fun delete(task: Task) {
+        viewModelScope.launch {
+            try {
+                repo.delete(task)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    application,
+                    "Insert fallido",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    fun deleteById(id: Int) {
+        viewModelScope.launch {
+            try {
+                repo.deleteById(id)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    application,
+                    "Delete fallido",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    fun getById(id: Int): Flow<Task> {
+        return repo.getById(id)
+    }
+
+
 }
